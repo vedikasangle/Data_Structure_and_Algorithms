c6da924b7bd498397ad6c91b37851f622ba0b21e
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-12-23 22:26:43 +0530

    first commit

---
+++ Basics of Programming/test1.cpp
@@ -0,0 +1,45 @@
+#include<iostream> //words ka matlab
+using namespace std; //syntax sahi hai ya nahi
+
+int main(){ //start
+    
+    //cout -> console out (console pe output)
+    //cin -> console in (console se input)
+
+    //words printing in " "
+    cout<<"coding";
+
+    //enter
+    cout<<'\n'; //imp
+    // cout<<endl;
+
+    //character printing in ' '
+    cout<<'A';
+
+    cout<<endl;
+
+    //numerical print
+    cout<<19;
+    cout<<endl;
+    // cout<<'\n';  ->same as endl
+    cout<<23.56;
+    cout<<endl;
+
+
+    //single line comment
+    
+    /* multiline comment
+    12
+    13
+    54
+    66*/
+
+    
+    //with the help of only one cout
+    cout<<"With the help of only one cout!";
+    cout<<endl;
+    cout<<19<<'\n'<<23.56<<'\n'<<'A'<<'\n'<<"coding"<<'\n';
+
+
+    return 0 ; //end
+}
