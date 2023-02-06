753f34017a5b60399abffa848500b2c2474aeb97
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2023-02-06 20:49:21 +0530

    Update selectionsort.cpp

--- searchingandsorting/selectionsort.cpp
+++ searchingandsorting/selectionsort.cpp
@@ -4,10 +4,23 @@ using namespace std ;
 
 int main(){
     int arr[] = {4,3,1,5,2};
-    int n = sizeof();
+    int n = sizeof(arr)/sizeof(int);
+    
+    //we assume the first element on the first index to be the smallest and then compare
+    
+    for(int pos = 0 ; pos<= n -2; pos++){
+    int minindex = pos ;//track index
+    for(int j = pos + 1 ; j <=n-1 ; j ++){
+    if(arr[j]<arr[minindex]){
+        minindex = j; //not writing arr[j] because we are keeping hold of index and not element
+        }
+      }
+      swap(arr[pos], arr[minindex]);
+    }
 
-
-
+    for(int i = 0 ; i <=n-1 ; i ++){
+        cout<<arr[i]<<" ";
+    }
 
     return 0;
 }
