2e7e05a1560b34fd203b3428276cd3f486fdba1a
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2023-02-06 22:34:06 +0530

    Update insertionsort.cpp

--- searchingandsorting/insertionsort.cpp
+++ searchingandsorting/insertionsort.cpp
@@ -4,6 +4,25 @@ using namespace std ;
 
 int main(){
     int arr[] = {4,3,1,5,2};
+    int n = sizeof(arr)/sizeof(int);
+    int j ;
+
+    for(int i = 1;i<=n-1;i++){
+        int ele = arr[i];
+        for(j = i -1; j>=0 ; j--){
+            if(ele<arr[j]){
+                arr[j+1] = arr[j];
+            }
+            else{
+                break;
+            }
+        }
+        arr[j+1] = ele;
+    }   
+
+    for(int i = 0  ; i<=n-1 ; i++){
+        cout<<arr[i]<<" ";
+    }
    
 
 
