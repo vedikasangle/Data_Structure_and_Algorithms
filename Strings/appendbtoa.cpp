ba7022885e12c1e95faec8887deb932a5d4bfb99
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2023-02-10 22:12:37 +0530

    Append b to a

---
+++ Strings/appendbtoa.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std ;
+
+append(char arr1[100], char arr2[100]{
+    int i = lengthofarr(arr1);
+    int j = 0 ;
+
+    while(j<)
+})
+
+
+
+int main(){
+    char arr1[100];
+    char arr2[100];
+
+
+    cin.getline(arr1,100);//Hello
+    cin.getline(arr2,100);//coding  
+
+    // arr1-->Hellocoding
+    // arr2 -->coding
+
+    append(arr1,arr2);
+
+ 
+
+    
+
+}
