39094eea6b1018a23738b592d0791e3dc8f70a98
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2023-02-12 08:54:01 +0530

    Update lengthofstring.cpp

--- Strings/lengthofstring.cpp
+++ Strings/lengthofstring.cpp
@@ -15,10 +15,20 @@ int lengthofarr(char arr[100]){
 int main(){
     char arr[100];
     cin.getline(arr,100);
+    // cin>>arr; //but in case of cin, how is it knowing that '\0' has occured => okay it is actually not seeking it, it is seeking space or '\n'
     cout<<"Length of array is "<<lengthofarr(arr)<<endl;
- 
 
-    
-
+    //getline working 
+    // char arr[100];
+    // char ch ; 
+    // ch = cin.get();
+    // int i = 0; 
+    // while(ch!='$'){
+    //     arr[i] = ch;
+    //     i++; 
+    //     ch = cin.get();
+    // }
+    // arr[i] = '\0'; //cin or even cin.getline puts \0 at the end 
+    // cout<<arr<<endl;
 
 }
