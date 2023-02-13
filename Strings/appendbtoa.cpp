2d4e12b26b09a583023f503d0e9c2d9705c85142
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2023-02-13 09:25:04 +0530

    Update appendbtoa.cpp

--- Strings/appendbtoa.cpp
+++ Strings/appendbtoa.cpp
@@ -1,13 +1,30 @@
 #include<iostream>
 using namespace std ;
 
-append(char arr1[100], char arr2[100]{
+int lengthofarr(char arr[100]){
+    int countofchar = 0 ;
+    
+    while(arr[countofchar]!='\0'){
+    countofchar++;
+    }
+    return countofchar;
+}
+
+
+void append(char arr1[100], char arr2[100]){
     int i = lengthofarr(arr1);
-    int j = 0 ;
+    int j = 0 ; 
 
-    while(j<)
-})
 
+    while(j<=lengthofarr(arr2)){
+    arr1[i] = arr2[j];
+    i++;
+    j++;
+    }
+
+    cout<<arr1<<endl;//HelloCoding
+    cout<<arr2<<endl;//Coding
+}
 
 
 int main(){
@@ -17,14 +34,12 @@ int main(){
 
     cin.getline(arr1,100);//Hello
     cin.getline(arr2,100);//coding  
-
+    
+    //output should be ==>
     // arr1-->Hellocoding
     // arr2 -->coding
 
     append(arr1,arr2);
 
- 
-
-    
-
+    return 0 ; 
 }
