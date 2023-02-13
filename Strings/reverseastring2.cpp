92867dcfbdb3b181613587e4715c77b1574d5166
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2023-02-13 18:51:01 +0530

    Update reverseastring2.cpp

--- Strings/reverseastring2.cpp
+++ Strings/reverseastring2.cpp
@@ -1,10 +1,40 @@
 #include<iostream>
 using namespace std ;
 
-//using two arrays
+//using two arrays 
+
+int lengthofarr(char arr[100]){
+    int countofchar = 0 ;
+    
+    while(arr[countofchar]!='\0'){
+    countofchar++;
+    }
+    return countofchar;
+}
+
+
+void reversestring(char arr[100]){
+    char temp[100];
+    int i = lengthofarr(arr) - 1; 
+    int j = 0 ; 
+
+    while(i>=0){
+        temp[j] = arr[i];
+        i--;
+        j++; 
+    }
+    temp[j] = '\0';
+    cout<<temp<<endl;
+    
+}
 
 
 int main(){
     char arr[100];
+    // char temp[100];
+
+    cin.getline(arr,100);
+
+    reversestring(arr);
   
 }
