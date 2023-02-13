56335b4eac8a6b36347841056cd3df8ab67d6b7a
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2023-02-13 09:25:53 +0530

    Update stringpalindromeornot.cpp

--- Strings/stringpalindromeornot.cpp
+++ Strings/stringpalindromeornot.cpp
@@ -1,15 +1,42 @@
 #include<iostream>
-using namespace std ;
+using namespace std ; 
 
+int lengthofarr(char arr[100]){
+    int countofchar = 0 ;
+    
+    while(arr[countofchar]!='\0'){
+    countofchar++;
+    }
+    return countofchar;
+}
 
+bool palindromeornot(char arr[100]){
+    int i = 0 ; 
+    int j = lengthofarr(arr) - 1 ;
+    
+    //loop
+    while(i<j){
+    if(arr[i] == arr[j]){
+        i++;
+        j--;
+    }
+    else{
+        return false ; 
+    }
+    }
 
-int main(){
+    return true ; 
+}
+
+int main (){
     char arr[100];
     cin.getline(arr,100);
-    palindromeornot(arr);
- 
 
-    
-
+    if(palindromeornot(arr) == true){
+        cout<<"Yes Palindrome"<<endl;
+    }
+    else{
+        cout<<"No Palindrome"<<endl;   
+    }
 
 }
