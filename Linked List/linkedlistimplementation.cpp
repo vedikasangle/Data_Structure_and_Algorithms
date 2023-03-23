43cb2b5fc3d0668e90556e9a3c7985c908d8cfbd
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2023-03-23 22:22:35 +0530

    Implementation

---
+++ Linked List/linkedlistimplementation.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+
+class node{
+
+    public : 
+    int data;
+    node*next;  // datatype*nameofpointer
+    node(int d){
+        data = d ; 
+        next = NULL; //initially no address going in it!!!
+    }
+
+
+};
+
+insertatfront(node*head, node*tail, int data){
+    ///case 1 ll is empty 
+    if()
+}
+
+int main(){
+    node*head = NULL;
+    node*tail = NULL;
+
+    //right now, head and tail is null, linkedlist is empty
+    
+    insertatfront(head,tail,d);
+
+
+
+    
+    
+    return 0;
+}
